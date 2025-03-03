9d03ed546976bad044f7660ba1a2cc2c8d659c19
zy <garythebob@gmail.com>
2025-03-04 02:12:45 +0800

    change config

--- src/my-website/docusaurus.config.ts
+++ src/my-website/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://awzhenyi.github.io/',
+  url: 'https://awzhenyi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/leetcode/',
   deploymentBranch: 'master',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
